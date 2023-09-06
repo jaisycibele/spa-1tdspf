8b7ec95ca949b2be4fb19560128d5d355d97f7b8
alecarlojesus <profalexandre.jesus@fiap.com.br>
2023-09-06 13:22:54 -0300

    Exemplo prox aula - Editar Produtos

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,40 +1,65 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 import { ListaProdutos } from '../components/ListaProdutos';
 
 export default function EditarProdutos() {
 
   document.title = "EDITAR PRODUTO";
 
+  const navigate = useNavigate();
+
   const {id} = useParams();
 
   const produtoRecuperado = ListaProdutos.filter((produto) => produto.id == id)[0]
 
+  const [produto, setProduto] = useState({
+    id: produtoRecuperado.id,
+    nome: produtoRecuperado.nome,
+    desc: produtoRecuperado.desc,
+    preco: produtoRecuperado.preco,
+  });
+
+  const handleChange = (event)=>{
+    event.preventDefault();
+    const {name, value} = event.target;
+    setProduto({...produto,[name]:value});
+  }
+
+  const handleSubmit = (event)=>{
+    event.preventDefault();
+    //Inserir o objeto editado na lista de produtos utilizados.
+    ListaProdutos.splice((id-1),1,produto);
+    navigate("/produtos");
+}
 
       
   return (
     <>
         <div>
-          <form>
+          <form onSubmit={handleSubmit}>
             <fieldset>
               <legend>Produto Selecionado</legend>
               <div>
                 <label htmlFor="idNome">Nome</label>
-                <input type="text" name="nome" id="idNome" defaultValue={produtoRecuperado.nome}/>
+                <input type="text" name="nome" id="idNome" value={produto.nome} onChange={handleChange}/>
               </div>
               <div>
                 <label htmlFor="idDesc">Descrição</label>
-                <input type="text" name="desc" id="idDesc" defaultValue={produtoRecuperado.desc}/>
+                <input type="text" name="desc" id="idDesc" value={produto.desc} onChange={handleChange}/>
               </div>
               <div>
                 <label htmlFor="idPreco">Preço</label>
-                <input type="text" name="preco" id="idPreco" defaultValue={produtoRecuperado.preco}/>
+                <input type="text" name="preco" id="idPreco" value={produto.preco} onChange={handleChange}/>
               </div>
               <div>
                 <button>EDITAR</button>
               </div>
             </fieldset>
           </form>
+
+          <div>VALOR DO USESTATE : {produto.nome}</div>
+          <div>VALOR DO USESTATE : {produto.desc}</div>
+          <div>VALOR DO USESTATE : {produto.preco}</div>
         </div>
     </>
     
