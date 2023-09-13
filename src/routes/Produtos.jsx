378dfd6d606a373926cd1e4d3e85ed8a34032afc
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-13 14:12:28 -0300

    iniciando o modal

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -3,8 +3,20 @@ import { ListaProdutos } from "../components/ListaProdutos";
 import {AiFillEdit as EditObj} from "react-icons/ai"
 import {RiDeleteBin2Fill as DelObj} from "react-icons/ri";
 import estilos from "./Produtos.module.css";
+import ModalData from "../components/ModalData";
+import {useState} from "react";
 
 export default function Produtos() {
+
+ const [status, setStatus] = useState({
+  open: false,
+  id:0,
+ })
+
+ const handleStatus = (status,id)=>{
+    setStatus({"open":status,"id":id})
+ }
+  
   return (
     <>
       <h1>Produtos Informáticos - FIAPO</h1>
@@ -27,7 +39,7 @@ export default function Produtos() {
               <td>{produto.nome}</td>
               <td>{produto.desc}</td>
               <td>{produto.preco}</td>
-              <td> <Link to={`/editar/produtos/${produto.id}`}><EditObj/></Link> | <Link to={`/excluir/produtos/${produto.id}`}><DelObj/></Link></td>
+              <td><button onClick={handleStatus(true,produto.id)}>Abrir Modal</button> <Link to={`/editar/produtos/${produto.id}`}><EditObj/></Link> | <Link to={`/excluir/produtos/${produto.id}`}><DelObj/></Link></td>
             </tr>
           ))}
         </tbody>
@@ -37,6 +49,11 @@ export default function Produtos() {
           </tr>
         </tfoot>
       </table>
+
+            <div>
+              <ModalData statusModal={status.open} idProduto={status.id}/>
+            </div>
+
     </>
   );
 }
