ffc905016e8a94ba0c210e64e8fd7bc932e2bfc6
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-13 13:14:14 -0300

    Finalizando o editar com useState, useParams, useNavigate.

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { ListaProdutos } from '../components/ListaProdutos';
 
@@ -10,24 +10,57 @@ export default function EditarProdutos() {
   const { id } = useParams();
 
   const produtoRecuperado = ListaProdutos.filter((produto) => produto.id == id)[0]
-      
+
+  const [produto, setProduto] = useState({
+    id: produtoRecuperado.id,
+    nome: produtoRecuperado.nome,
+    desc: produtoRecuperado.desc,
+    preco: produtoRecuperado.preco,
+  })
+  
+  const handleChange = (e)=>{
+    //Destructuring
+    const {name,value} = e.target;
+    //Populando o state produto com as propriedades dos inputs que estão sendo ativados, utilizando SPREAD.
+    setProduto({...produto,[name]:value});
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    let indice;
+    //Recuperando o índice do produto alterado com forEach.
+    // ListaProdutos.forEach((item,index)=>{
+    //   if(item === produto){
+    //     indice = index;
+    //   }
+    // });
+
+    //Ou utilizando o método indexOf
+    indice = ListaProdutos.indexOf(item => item.id == produto.id);
+
+    //Alterando o produto na lista com o método splice()
+    ListaProdutos.splice(indice,1,produto);
+
+    //Redirecionando o usuáio para a página de produtos!
+    navigate('/produtos');
+  }
   return (
     <>
         <div>
-          <form>
+          <form onSubmit={handleSubmit}>
             <fieldset>
               <legend>Produto Selecionado</legend>
               <div>
                 <label htmlFor="idNome">Nome</label>
-                <input type="text" name="nome" id="idNome" defaultValue={produtoRecuperado.nome}/>
+                <input type="text" name="nome" id="idNome" value={produto.nome} onChange={handleChange}/>
               </div>
               <div>
                 <label htmlFor="idDesc">Descrição</label>
-                <input type="text" name="desc" id="idDesc" defaultValue={produtoRecuperado.desc}/>
+                <input type="text" name="desc" id="idDesc" value={produto.desc} onChange={handleChange}/>
               </div>
               <div>
                 <label htmlFor="idPreco">Preço</label>
-                <input type="text" name="preco" id="idPreco" defaultValue={produtoRecuperado.preco}/>
+                <input type="text" name="preco" id="idPreco" value={produto.preco} onChange={handleChange}/>
               </div>
               <div>
                 <button>EDITAR</button>
