9c007eb11e88ea08aa73ae7966deeb9406d016a3
jaisycibele <jaisycibele@gmail.com>
2023-09-28 10:53:55 -0300

    Iniciando a construçao do ModalInserir

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,10 +1,9 @@
 import { Link } from "react-router-dom";
-import { ListaProdutos } from "../components/ListaProdutos";
 import { AiFillEdit as EditObj } from "react-icons/ai";
 import { RiDeleteBin2Fill as DelObj } from "react-icons/ri";
 import estilos from "./Produtos.module.css";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import ModalInserir from "../components/ModalInserir/ModalInserir";
 
 export default function Produtos() {
   document.title = "Lista de Produtos";
@@ -20,19 +19,24 @@ fetch("http://localhost:5000/produtos", {
       },
     })
       .then((response) => {
-        console.log("o status da requisição / request http: " + response.status);
-      
-      response.json())
-      
+        console.log("O Status da requisição/request HTTP : " + response.status);
+        return response.json()
+      })
       .then((data) => {
         setListaProdutosLocal(data);
       })
       .catch((err) => console.log(err));
   }, []);
 
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <h1>Produtos Informáticos - FIAPO</h1>
+
+      { open ? <ModalInserir open={open} setOpen={setOpen}/> : "" }
+
+      <button onClick={()=> setOpen(true)}>OPEN-MODAL</button>
 
       <table className={estilos.tblEstilo}>
         <thead>
